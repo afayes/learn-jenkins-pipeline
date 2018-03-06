d35b4c26690f5613125db89ffc1da2eee17dd7b3
abulfayes <afayes@hotmail.co.uk>
2018-03-06 14:50:41 +0000

    added a unit test

---
+++ src/test/java/com/abul/fayes/simplespringbootwebapp/ControllerTest.java
@@ -0,0 +1,15 @@
+package com.abul.fayes.simplespringbootwebapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * todo add comments.
+ */
+public class ControllerTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
